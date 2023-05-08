3ee145e972e6f8f2ce72acb9723bc99bfed45b94
carlos.hoffmann <carlos.hoffmann@foursys.com.br>
2023-05-08 17:44:34 -0300

    Primeiro commit do app Dieta do Tola

---
+++ app/src/main/java/br/com/hoffmann/dietadotola/Cadastro.java
@@ -0,0 +1,102 @@
+package br.com.hoffmann.dietadotola;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Cadastro extends AppCompatActivity {
+
+    private Button botao_cadastro;
+    private EditText nomeCadastro, emailCadastro, senhaCadastro;
+    String usuarioID;
+    String[] mensagens = {"É necessário preencher todos os campos obrigatórios",
+            "Cadastro realizado com sucesso."};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        iniciaComponentes();
+
+        botao_cadastro.setOnClickListener(view -> {
+
+            String nome = nomeCadastro.getText().toString();
+            String email = emailCadastro.getText().toString();
+            String senha = senhaCadastro.getText().toString();
+
+            if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
+                Toast.makeText(Cadastro.this, mensagens[0], Toast.LENGTH_LONG).show();
+            } else {
+                cadastrarUsuario();
+            }
+        });
+    }
+
+    private void cadastrarUsuario() {
+        String email = emailCadastro.getText().toString();
+        String senha = senhaCadastro.getText().toString();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+
+                salvarDadosUsuario();
+
+                Toast.makeText(Cadastro.this, mensagens[1], Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Cadastro.this, Cadastro.class);
+                startActivity(intent);
+            } else {
+                String erro;
+                try {
+                    throw Objects.requireNonNull(task.getException());
+                } catch (FirebaseAuthWeakPasswordException e) {
+                    erro = "Digite uma senha com no minimo 6 caracteres";
+                } catch (FirebaseAuthUserCollisionException e) {
+                    erro = "Este email ja existe na base de dados";
+                } catch (FirebaseAuthInvalidCredentialsException e) {
+                    erro = "E-mail invalido";
+                } catch (Exception e) {
+                    erro = "Falha de comunicação com o Firebase.";
+                }
+                Toast.makeText(Cadastro.this, erro, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void salvarDadosUsuario() {
+        String nome = nomeCadastro.getText().toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> usuarios = new HashMap<>();
+        usuarios.put("nome", nome);
+
+        usuarioID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(unused -> Log.d("db", "Sucesso ao salvar os dados")).addOnFailureListener(e -> Log.d("db_error", "Erro ao salvar os dados" + e.getMessage()));
+    }
+
+    private void iniciaComponentes() {
+        botao_cadastro = findViewById(R.id.botao_cadastro);
+        nomeCadastro = findViewById(R.id.nome_cadastro);
+        emailCadastro = findViewById(R.id.email_cadastro);
+        senhaCadastro = findViewById(R.id.senha_cadastro);
+    }
+}
---
+++ app/src/main/java/br/com/hoffmann/dietadotola/Login.java
@@ -0,0 +1,100 @@
+package br.com.hoffmann.dietadotola;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
+public class Login extends AppCompatActivity {
+
+    private TextView telaCadastro;
+    private Button botaoEntrar;
+    private EditText emailCadastro, senhaCadastro;
+    private ProgressBar progressBar;
+
+    String[] mensagens = {"É necessário preencher todos os campos obrigatórios",
+            "Login efetuado com sucesso."};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        iniciarComponentes();
+
+        botaoEntrar.setOnClickListener(view -> {
+            String email = emailCadastro.getText().toString();
+            String senha = senhaCadastro.getText().toString();
+
+            if (email.isEmpty() || senha.isEmpty()) {
+                Toast.makeText(Login.this, mensagens[0], Toast.LENGTH_LONG).show();
+            } else {
+                logarUsuario(email, senha);
+            }
+        });
+
+        telaCadastro.setOnClickListener(view -> {
+            Intent intent = new Intent(Login.this, Cadastro.class);
+            startActivity(intent);
+        });
+    }
+
+    private void logarUsuario(String email, String senha) {
+
+        String email1 = emailCadastro.getText().toString();
+        String senha1 = senhaCadastro.getText().toString();
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                progressBar.setVisibility(View.VISIBLE);
+
+                new Handler().postDelayed(this::telaPrincial, 2000);
+            } else {
+                String erro;
+                try {
+                    throw Objects.requireNonNull(task.getException());
+                } catch (Exception e) {
+                    erro = "Erro ao logar o usuario";
+                }
+                Toast.makeText(Login.this, erro, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void telaPrincial() {
+        Intent intent = new Intent(Login.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void iniciarComponentes() {
+        emailCadastro = findViewById(R.id.email_login);
+        senhaCadastro = findViewById(R.id.senha_login);
+        botaoEntrar = findViewById(R.id.botao_entrar);
+        telaCadastro = findViewById(R.id.texto_cadastro);
+        progressBar = findViewById(R.id.progress_bar);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            telaPrincial();
+        }
+    }
+}
